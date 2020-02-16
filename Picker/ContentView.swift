4bee55e15f78f5a7b284184c1223aab094ebe175
Jan Hovland <jan.hovland@lyse.net>
2020-02-16 10:22:52 +0100

    Initial Commit

---
+++ Picker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Picker
+//
+//  Created by Jan Hovland on 16/02/2020.
+//  Copyright © 2020 Jan Hovland. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
