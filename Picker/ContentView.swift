dc9dcd522b952d3df0f0ac9c49d5a930026b4bb5
Jan Hovland <jan.hovland@lyse.net>
2020-02-16 11:55:12 +0100

    Update

--- Picker/ContentView.swift
+++ Picker/ContentView.swift
@@ -39,3 +39,49 @@ struct ContentView: View {
         }
     }
 }
+
+struct MultiPicker: View  {
+
+    typealias Label = String
+    typealias Entry = String
+
+    let data: [ (Label, [Entry]) ]
+    @Binding var selection: [Entry]
+
+    var body: some View {
+        GeometryReader { geometry in
+            HStack {
+                ForEach(0..<self.data.count) { column in
+                    Picker(self.data[column].0, selection: self.$selection[column]) {
+                        ForEach(0..<self.data[column].1.count) { row in
+                            Text(verbatim: self.data[column].1[row])
+                            .tag(self.data[column].1[row])
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    .frame(width: geometry.size.width / CGFloat(self.data.count), height: geometry.size.height)
+                    .clipped()
+                }
+            }
+        }
+    }
+}
+
+struct ContentViewW: View {
+
+    @State var data: [(String, [String])] = [
+        ("One", Array(0...10).map { "\($0)" }),
+        ("Two", Array(20...40).map { "\($0)" }),
+        ("Three", Array(100...200).map { "\($0)" })
+    ]
+    @State var selection: [String] = [0, 20, 100].map { "\($0)" }
+
+    var body: some View {
+        VStack(alignment: .center) {
+            Text(verbatim: "Selection: \(selection)")
+            MultiPicker(data: data, selection: $selection).frame(height: 300)
+        }
+    }
+
+}
+
